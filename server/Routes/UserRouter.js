343d5a44ed7519a2e57a8bb56c4f263f79fbab9e
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-11-17 23:53:55 -0300

    ChatApi

--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,8 +1,16 @@
 const express = require("express")
-const {registerUser} = require("../Controllers/userController")
+const {registerUser,loginUser,findUser,getUser} = require("../Controllers/userController")
+
 
 const router = express.Router();
 
-router.get("/register",registerUser)
+router.get("/find/:userId",findUser)
+router.get("/",getUser)
+router.post("/register",registerUser)
+router.post("/login",loginUser)
+
+
+
+
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -2,19 +2,23 @@ const express = require("express");
 const cors = require("cors")
 const mongoose = require("mongoose")
 const userRoute = require("./Routes/UserRouter")
+const chatRoute = require("./Routes/chatRoute")
+const messageRoute = require("./Routes/messageRoute")
 
 const app = express()
 require("dotenv").config()
 
 app.use(express.json())
 app.use(cors())
-app.use("/api/user", userRoute);
-
+app.use("/api/users", userRoute);
+app.use("/api/chats",chatRoute);
+app.use("/api/messages",messageRoute);
 
 const port = 5000;
 app.listen(port,(req,res)=>{
     console.log(`Server Running on port : ${port}`);
 });
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri,{}).then(()=>console.log("MongoDb connection established"))
 .catch((error) => console.log("MongoDB Failed:",error.message));
